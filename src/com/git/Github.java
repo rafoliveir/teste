dbf3417a18a3e7a57655b443bbd75d1b9832d7f4
rafoliveir <rafol@192.168.44.15>
2015-11-10 21:03:23 -0200

    Rafael Repository

---
+++ src/com/git/Github.java
@@ -0,0 +1,33 @@
+package com.git;
+
+import java.util.Scanner;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String nome;
+		
+		int num1;
+		int num2;
+		int soma;
+		
+		System.out.print("Digite um número: ");
+		num1 = entrada.nextInt();
+		
+		System.out.print("Digite outro numero para somar: ");
+		num2 = entrada.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Resultado: " +soma);
+		
+		
+		
+		
+	}
+
+}
